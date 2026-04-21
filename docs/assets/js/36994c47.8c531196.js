5b3b07347ec3c6baa31d1205a4fcfd1e504d17a9
Diego Gómez <2833316742101@ingenieria.usac.edu.gt>
2026-04-21 00:26:55 -0600

    Add files via upload

---
+++ docs/assets/js/36994c47.8c531196.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkproyecto2_asertiva=self.webpackChunkproyecto2_asertiva||[]).push([["191"],{5516(e){e.exports=JSON.parse('{"name":"docusaurus-plugin-content-blog","id":"default"}')}}]);
